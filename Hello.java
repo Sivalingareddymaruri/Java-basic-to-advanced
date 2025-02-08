9eb7b6b40006560001a5c5c243a8326e1284baf7
Sivalingareddymaruri <108017045+Sivalingareddymaruri@users.noreply.github.com>
2025-02-08 15:57:33 +0530

    Add files via upload

---
+++ Hello.java
@@ -0,0 +1,15 @@
+package Java_basic_to_advanced;
+
+
+public class Hello
+{
+    public static void main(String a[])
+     {
+        int num1 = 3;
+        int num2=4;
+        int result=num1+num2;
+        System.out.println("Hello World");
+        System.out.println(result);
+        System.out.println(6+5);
+    }
+}
