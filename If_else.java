7db8044bf80f3199c57116bd53dac8800428b265
Sivalingareddy Maruri <108017045+Sivalingareddymaruri@users.noreply.github.com>
2025-02-10 13:33:45 +0530

    Add files via upload

---
+++ If_else.java
@@ -0,0 +1,16 @@
+public class If_else {
+    public static void main(String[] args) {
+        int x=18;
+        int y=7;
+        if(x>y) {
+            System.out.println("Hello ");
+            System.out.println("Guys!");  //we use curly braces when we have ore than one statements
+        }
+        else
+            System.out.println("Bye");
+        if(true){  //if(false)->it will give output
+            System.out.println("Hello,The condition was true");
+        }
+    }
+    
+}
