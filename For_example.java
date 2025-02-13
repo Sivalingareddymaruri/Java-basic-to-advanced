2ea134446ed4389c653c652399bfe43c6744059e
Sivalingareddy Maruri <108017045+Sivalingareddymaruri@users.noreply.github.com>
2025-02-13 10:35:46 +0530

    Add files via upload

---
+++ For_example.java
@@ -0,0 +1,15 @@
+public class For_example {
+    public static void main(String[] args) 
+    {
+        for (int i=0;i<=7;i++)
+        {
+            System.out.println("DAY "+i);
+
+            for(int j=9;j<18;j++)
+            {
+                System.out.println(j+"am"+"-"+(j+1)+"am");
+            }
+        }
+    }
+    
+}
---
+++ For_loop.java
@@ -0,0 +1,9 @@
+public class For_loop {
+    public static void main(String[] args) 
+    {
+        for(int i=0;i<=4;i++)  //for decrement(i=4;i<=1;i--)
+        {
+            System.out.println("Hi"+i);
+        }
+    }
+}
